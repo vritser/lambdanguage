cc1be5acafc6c4ebe15727f2feed0659f23725a2
vritser <vritser@163.com>
2020-08-04 11:25:13 +0800

    constructs list and test

--- test/evaluate.test.ts
+++ test/evaluate.test.ts
@@ -1,20 +1,38 @@
 import { expect } from 'chai';
-import evaluate from '../src/envaluate';
 import Environment from '../src/environment';
+import evaluate from '../src/evaluate';
 import InputStream from '../src/input_stream';
 import Parser from '../src/parser';
 import TokenStream from '../src/token_stream';
 
 describe('evaluate', () => {
-  const code = `sum = lambda(x, y) x + y; print(sum(2, 3)); sum(2, 3);`;
 
-  const ast = new Parser(new TokenStream(new InputStream(code))).parse();
   const globalEnv = new Environment();
   globalEnv.def("print", console.log);
 
   it('sum of 2 and 3 should be 5', () => {
+    const code = `sum = λ(x, y) x + y; print(sum(2, 3)); sum(2, 3);`;
+    const ast = new Parser(new TokenStream(new InputStream(code))).parse();
     const sum = evaluate(ast, globalEnv);
     expect(sum).to.eq(5);
+  });
+
+  it('constructs list', () => {
+    const code = `
+
+cons = λ(a, b) λ(f) f(a, b);
+car = λ(cell) cell(λ(a, b) a);
+cdr = λ(cell) cell(λ(a, b) b);
+NIL = λ(f) f(NIL, NIL);
+
+x = cons(1, cons(2, cons(3, cons(4, cons(5, NIL)))));
+print(car(x));
+print(car(cdr(cdr(x))));
+print(car(cdr(cdr(cdr(cdr(x))))));
+
+`;
+    const ast = new Parser(new TokenStream(new InputStream(code))).parse();
+    evaluate(ast, globalEnv);
   });
 
 });
